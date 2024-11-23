f063c5f0e7c360110258384dff5f8251ff498676
Bojan Ludajic <bojan.ludajic@gmail.com>
2024-11-24 00:34:30 +0100

    added toString() for AST nodes

--- src/main/kotlin/parser/Node.kt
+++ src/main/kotlin/parser/Node.kt
@@ -1,7 +1,31 @@
 package com.example.parser
 
 sealed class ASTNode{
-    data class Assign(val name: String, val value: String) : ASTNode()
-    data class Print(val name: String) : ASTNode()
-    data class Scope(val body: List<ASTNode>) : ASTNode()
+    data class Assign(val name: String, val value: String) : ASTNode() {
+        override fun toString(): String {
+            return "Assign: $name = $value"
+        }
+    }
+    data class Print(val name: String) : ASTNode() {
+        override fun toString(): String {
+            return "Print: $name"
+        }
+    }
+    data class Scope(val body: List<ASTNode>) : ASTNode() {
+        override fun toString(): String {
+            return buildString {
+                appendLine("Scope {")
+                body.forEach {
+                    appendLine(indentations(it.toString()))
+                }
+                append("}")
+            }
+        }
+
+        private fun indentations(name: String): String {
+            val indentation = "    ".repeat(1)
+            return name.lineSequence()
+                .joinToString("\n") { "$indentation$it" }
+        }
+    }
 }
